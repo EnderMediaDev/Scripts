058d41e3bfc9ad5a8f31d57ecdbea1e0fc3688d2
Caleb King <ciak2009@gmail.com>
2015-03-02 12:07:08 -0500

    Started clever text

---
+++ generateserver.sh
@@ -0,0 +1,7 @@
+echo "Welcome to EnderMedia Server Packager version 0.0.1"
+echo "Since you are a lazy son of a gun, I'm going to do everything for you"
+echo "If you can pry your fingers up for 2 small questions that would be great"
+echo "After that you can be lazy while I do the rest, okay here we go!"
+echo "Enter some text > "
+read text
+echo "You entered: $text"
