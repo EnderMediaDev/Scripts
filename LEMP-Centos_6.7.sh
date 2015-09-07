5572be2ae9d780857d55a3d4519de91b80cad28b
Caleb King <calebking@Calebs-MacBook-Pro.local>
2015-09-07 13:48:12 -0400

    Add sexy talk

--- LEMP-Centos_6.7.sh
+++ LEMP-Centos_6.7.sh
@@ -3,18 +3,26 @@ normal=`tput sgr0`
 
 setup() {
 
-echo "Welcome to the EnderMedia Server Deployment Script"
-echo "PLEASE ENSURE YOU ARE RUNNING THIS AS ROOT"
-
+echo "##### Welcome to the EnderMedia Server Deployment Script"
+echo "##### PLEASE ENSURE YOU ARE RUNNING THIS AS ROOT"
+echo ""
 yum -y install epel-release
 rpm -Uvh https://mirror.webtatic.com/yum/el6/latest.rpm
 yum -y install perl perl-Net-SSLeay openssl perl-IO-Tty wget
+echo ""
+echo "##### Starting Webmin Installation..."
+echo ""
 wget -nv http://prdownloads.sourceforge.net/webadmin/webmin-1.760-1.noarch.rpm
 yum -y install yum-plugin-replace
 rpm -U webmin-1.760-1.noarch.rpm
+echo ""
+echo "##### Installing NGINX, PHP and MYSQL"
+echo ""
 yum -y install nginx
 service nginx start
 chkconfig nginx on
+mkdir /usr/share/nginx/sites-enabled
+mkdir /usr/share/nginx/sites-available
 yum -y install php56w-fpm php56w-common php56w-cli php56w-gd php56w-imap php56w-ldap php56w-odbc php56w-pear php56w-xml php56w-xmlrpc php56w-mbstring php56w-mcrypt php56w-mssql php56w-snmp php56w-soap php56w-tidy php-php-gettext apr-util-ldap mailcap
 service php-fpm start
 chkconfig php-fpm on
@@ -34,19 +42,28 @@ sudo < /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c10
 echo ""
 echo "Starting installation..."
 /usr/bin/mysql_secure_installation
+echo ""
+echo "##### Restarting services..."
+echo ""
 service nginx restart
 service php-fpm restart
 service mysqld restart
-
+echo ""
+echo "##### Installing phpMyAdmin..."
+echo ""
 rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
 yum -y --enablerepo=remi install phpMyAdmin
-
-mkdir /usr/share/nginx/sites-enabled
-mkdir /usr/share/nginx/sites-available
-
+echo ""
+echo "##### Adding port 80 and 8080 to firewall."
+echo ""
 iptables -A INPUT -m state --state NEW -p tcp --dport 80 -j ACCEPT
 iptables -A INPUT -m state --state NEW -p tcp --dport 8080 -j ACCEPT
 /sbin/service iptables save
+echo ""
+echo "##### INSTALLATION COMPLETE"
+echo "##### THANK YOU FOR USING CIAK2009 LEMP SCRIPT"
+echo "##### GOODBYE MY DEAR FRIEND, I WILL MISS YOU."
+echo ""
 }
 
 setup 2>&1 | tee install-log.txt
