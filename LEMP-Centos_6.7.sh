6b7cac9e62b0223c044662a1aba6c40af4c865fc
Caleb King <calebking@Calebs-MacBook-Pro.local>
2015-09-07 00:29:38 -0400

    Sterf

--- LEMP-Centos_6.7.sh
+++ LEMP-Centos_6.7.sh
@@ -9,6 +9,7 @@ echo "PLEASE ENSURE YOU ARE RUNNING THIS AS ROOT"
 yum -y install epel-release
 rpm -Uvh https://mirror.webtatic.com/yum/el6/latest.rpm
 wget http://prdownloads.sourceforge.net/webadmin/webmin-1.760-1.noarch.rpm
+yum -y install yum-plugin-replace
 yum -y install perl perl-Net-SSLeay openssl perl-IO-Tty
 rpm -U webmin-1.760-1.noarch.rpm
 yum -y install nginx
@@ -20,14 +21,14 @@ chkconfig php-fpm on
 service php-fpm restart
 sed -i.bakuser 's|user = apache|user = nginx|' /etc/php-fpm.d/*.conf
 sed -i.baktmp 's|/var/lib/php/session/|/tmp|' /etc/php-fpm.d/*.conf
-yum -y remove mysql-*
+yum -y replace mysql-libs --replace-with mysql55w-libs
 yum -y install mysql55w-server
 yum -y install php56w-mysqlnd
 service mysqld start
 chkconfig mysqld on
 echo "Getting ready to start MySQL setup process."
-randompass=sudo < /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c10
-echo "Please use ${bold}$randompass"
+echo "Please use this password:" && randompass=sudo < /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c10
+echo "Starting installation"
 /usr/bin/mysql_secure_installation
 service nginx restart
 service php-fpm restart
